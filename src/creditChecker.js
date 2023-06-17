37e07d08660c0272cb7209ecf2cd8e71daeb5e6a
273* <keita.y310@icloud.com>
2023-06-17 15:13:07 +0900

    first commit

---
+++ src/creditChecker.js
@@ -0,0 +1,192 @@
+var drop = document.getElementById("drop");
+function handleDrop(e) {
+  $("#json-result").text("Transfering file...");
+  e.stopPropagation();
+  e.preventDefault();
+  var files = e.dataTransfer.files;
+  var f = files[0];
+  {
+    var reader = new FileReader();
+    var name = f.name;
+    reader.onload = function (e) {
+      var data = e.target.result;
+      $("#json-result").text("Parsing PDF...");
+      parse_content(data); //btoa(arr));
+    };
+    reader.readAsArrayBuffer(f);
+  }
+}
+
+function handleFile(e) {
+  $("#json-result").text("Transfering file...");
+  var files = e.target.files;
+  var f = files[0];
+  {
+    var reader = new FileReader();
+    var name = f.name;
+    reader.onload = function (e) {
+      var data = e.target.result;
+      $("#json-result").text("Parsing PDF...");
+      parse_content(data); //btoa(arr));
+    };
+    reader.readAsArrayBuffer(f);
+  }
+}
+document
+  .getElementById("pdf-file")
+  .addEventListener("change", handleFile, false);
+
+function handleDragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  e.dataTransfer.dropEffect = "copy";
+}
+
+if (drop.addEventListener) {
+  drop.addEventListener("dragenter", handleDragover, false);
+  drop.addEventListener("dragover", handleDragover, false);
+  drop.addEventListener("drop", handleDrop, false);
+}
+
+//
+// If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
+//
+
+var array_to_csv = function (tables) {
+  return tables
+    .map(function (rows) {
+      return rows
+        .map(function (v) {
+          if ("undefined" === typeof v || v === null) {
+            return v;
+          }
+          if (v.indexOf('"')) {
+            v = v.replace(/"/g, '""');
+          }
+          if (
+            v.indexOf('"') >= 0 ||
+            v.indexOf("\n") >= 0 ||
+            v.indexOf(",") >= 0
+          ) {
+            v = '"' + v + '"';
+          }
+          return v;
+        })
+        .join(",");
+    })
+    .join("\n");
+};
+$("#csv-select").change(function () {
+  $("#csv-result").val(
+    array_to_csv($("#csv-select option:selected").data("data").tables)
+  );
+});
+
+pdf_table_extractor_progress = function (result) {
+  $("#json-result").text(
+    "Parsing PDF, progress: " +
+      result.currentPages +
+      " / " +
+      result.numPages +
+      " pages"
+  );
+};
+
+var parse_content = function (content) {
+  PDFJS.workerSrc = "../src/pdf.worker.js";
+  // PDFJS.cMapUrl = "/cmaps/";
+  // PDFJS.cMapPacked = true;
+
+  PDFJS.getDocument(content)
+    .then(pdf_table_extractor)
+    .then(function (result) {
+      // JSON output
+      document.getElementById("json-result").innerText = JSON.stringify(result);
+      console.log(result.pageTables[result.currentPages - 1].tables);
+      var mainData = result.pageTables[result.currentPages - 1].tables[0];
+      var GPAData = result.pageTables[result.currentPages - 3];
+      mainDataProcessAndDisplay(mainData);
+      GPADataProcess(GPAData);
+      // const fileName = document.getElementById("pdf-file").files[0];
+      // console.log(fileName.name);
+      // HTML output
+      // CSV output
+      $("#csv-select").html("");
+      $("#html-result").html("");
+
+      var all_tables = [];
+      while ((page_tables = result.pageTables.shift())) {
+        if (
+          !$('input:checkbox[name="merge-table"]').is(":checked") &&
+          !$('input:checkbox[name="merge-table-remove-first-line"]').is(
+            ":checked"
+          )
+        ) {
+          $("#html-result").append(
+            $("<h3 id= 'sub-table'></h3>").text("Page " + page_tables.page)
+          );
+        }
+
+        $("#csv-select").append(
+          $("<option></option>")
+            .text("Page " + page_tables.page)
+            .data("data", page_tables)
+        );
+        if (
+          $('input:checkbox[name="merge-table-remove-first-line"]').is(
+            ":checked"
+          ) &&
+          page_tables.page > 1
+        ) {
+          all_tables = all_tables.concat(page_tables.tables.slice(1));
+        } else {
+          all_tables = all_tables.concat(page_tables.tables);
+        }
+        //table_dom = $("<table></table>").attr("border", 1);
+        if (result.pageTables == 0)
+          table_dom = $("<table id= 'sub-table'></table>").attr("border", 1);
+        else table_dom = $("<table id= 'sub-table'></table>").attr("border", 1);
+        var tables = page_tables.tables;
+        var merge_alias = page_tables.merge_alias;
+        var merges = page_tables.merges;
+
+        for (var r = 0; r < tables.length; r++) {
+          if (
+            $('input:checkbox[name="merge-table-remove-first-line"]').is(
+              ":checked"
+            ) &&
+            page_tables.page != 1 &&
+            r == 0
+          ) {
+            continue;
+          }
+          tr_dom = $("<tr></tr>");
+          for (var c = 0; c < tables[r].length; c++) {
+            r_c = [r, c].join("-");
+            if (merge_alias[r_c]) {
+              continue;
+            }
+
+            td_dom = $("<td></td>");
+            if (merges[r_c]) {
+              if (merges[r_c].width > 1) {
+                td_dom.attr("colspan", merges[r_c].width);
+              }
+              if (merges[r_c].height > 1) {
+                td_dom.attr("rowspan", merges[r_c].height);
+              }
+            }
+            td_dom.text(tables[r][c]);
+            tr_dom.append(td_dom);
+          }
+          table_dom.append(tr_dom);
+        }
+        $("#html-result").append(table_dom);
+      }
+      $("#csv-select").append(
+        $("<option></option>").text("all").data("data", { tables: all_tables })
+      );
+      $("#csv-select").change();
+    });
+};
---
+++ src/main.js
@@ -0,0 +1,327 @@
+//進捗バー，GPAの表示
+document.getElementById("bar").style.display = "none";
+document.getElementById("GPA").style.display = "none";
+//各学部学科の卒業要件単位 = [教養必修, 教養選択, 専門必修, 専門選択, 選択共通, 随意科目]
+var tanni = {
+  zyouhou: [10, 16, 48, 40, 10, 0],
+  keizai: [14, 16, 26, 52, 16, 0],
+};
+//データ表示
+var Flg = 0;
+function mainDataProcessAndDisplay(data) {
+  if (Flg == 0) {
+    makeTableAndBar(data);
+    Flg = 1;
+  } else if (Flg == 1) {
+    reset();
+    for (var i = 0; i < 4; i++) $("#curriculum").find("tr").slice(i).remove();
+    makeTableAndBar(data);
+  }
+}
+var GPAData = [];
+//GPAを管理する関数
+function GPADataProcess(data) {
+  for (var i = 1; i < data.tables.length; i++)
+    GPAData.push(Number(data.tables[i][1]));
+  console.log(GPAData);
+  document.getElementById("GPA").style.display = "block";
+  document.getElementById("GPATitle").innerHTML =
+    "通算GPA(" + GPAData[GPAData.length - 1] + "/4.0)";
+  document.getElementById("barGPA").value = GPAData[GPAData.length - 1];
+}
+//テーブル生成
+var kyoyoHissyu = 0;
+var kyoyoSentaku = 0;
+var senmonHissyu = 0;
+var senmonSentaku = 0;
+var sentakuKyotu = 0;
+var zuiiKamoku = 0;
+var gokeiTanni = 0;
+var GraduationSum = 0;
+var EarnedCreditsSum = 0;
+var TakingCoursesCreditsSum = 0;
+var sintyokuData = [];
+var EarnedCreditsData = [];
+var TakingCoursesCreditsData = [];
+var tanniTmp = [];
+function makeTableAndBar(data) {
+  EarnedCreditsData = [
+    Number(data[1].split("\n")[1]),
+    Number(data[1].split("\n")[2]),
+    Number(data[1].split("\n")[17]),
+    Number(data[1].split("\n")[18]),
+  ];
+  TakingCoursesCreditsData = [
+    Number(data[3].split("\n")[1]),
+    Number(data[3].split("\n")[2]),
+    Number(data[3].split("\n")[17]),
+    Number(data[3].split("\n")[18]),
+  ];
+
+  if (data[1].split("\n").length == 28) {
+    //情報工学科
+    tanniTmp = tanni.zyouhou;
+    EarnedCreditsData.push(Number(data[1].split("\n")[24]));
+    EarnedCreditsData.push(Number(data[1].split("\n")[25]));
+    TakingCoursesCreditsData.push(Number(data[1].split("\n")[24]));
+    TakingCoursesCreditsData.push(Number(data[1].split("\n")[25]));
+  } else if (data[1].split("\n").length == 34) {
+    //経済学部
+    tanniTmp = tanni.keizai;
+    EarnedCreditsData.push(Number(data[1].split("\n")[30]));
+    EarnedCreditsData.push(Number(data[1].split("\n")[31]));
+    TakingCoursesCreditsData.push(Number(data[1].split("\n")[30]));
+    TakingCoursesCreditsData.push(Number(data[1].split("\n")[31]));
+  } else {
+    alert("このPDFファイルは読み込めません．");
+    window.location.href = window.location.href;
+  }
+
+  console.log(data[1].split("\n"));
+  for (var i = 0; i < tanniTmp.length; i++) {
+    GraduationSum = GraduationSum + tanniTmp[i];
+    EarnedCreditsSum = EarnedCreditsSum + EarnedCreditsData[i];
+    TakingCoursesCreditsSum =
+      TakingCoursesCreditsSum + TakingCoursesCreditsData[i];
+  }
+
+  console.log(tanniTmp);
+
+  tanniTmp.push(GraduationSum);
+  EarnedCreditsData.push(EarnedCreditsSum);
+  TakingCoursesCreditsData.push(TakingCoursesCreditsSum);
+
+  let table = document.createElement("table");
+  let thead = document.createElement("thead");
+  let tbody = document.createElement("tbody");
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  document.getElementById("curriculum").appendChild(table);
+  let row_1 = document.createElement("tr");
+  let heading_1 = document.createElement("th");
+  heading_1.innerHTML = "科目分類";
+  let heading_2 = document.createElement("th");
+  heading_2.innerHTML = "教養必修";
+  let heading_3 = document.createElement("th");
+  heading_3.innerHTML = "教養選択";
+  let heading_4 = document.createElement("th");
+  heading_4.innerHTML = "専門必修";
+  let heading_5 = document.createElement("th");
+  heading_5.innerHTML = "専門選択";
+  let heading_6 = document.createElement("th");
+  heading_6.innerHTML = "選択共通	";
+  let heading_7 = document.createElement("th");
+  heading_7.innerHTML = "随意科目";
+  let heading_8 = document.createElement("th");
+  heading_8.innerHTML = "合計単位	";
+
+  row_1.appendChild(heading_1);
+  row_1.appendChild(heading_2);
+  row_1.appendChild(heading_3);
+  row_1.appendChild(heading_4);
+  row_1.appendChild(heading_5);
+  row_1.appendChild(heading_6);
+  row_1.appendChild(heading_7);
+  row_1.appendChild(heading_8);
+  thead.appendChild(row_1);
+
+  let row_2 = document.createElement("tr");
+  let row_2_data_1 = document.createElement("td");
+  row_2_data_1.innerHTML = "卒業要件単位";
+  let row_2_data_2 = document.createElement("td");
+  row_2_data_2.innerHTML = tanniTmp[0];
+  let row_2_data_3 = document.createElement("td");
+  row_2_data_3.innerHTML = tanniTmp[1];
+  let row_2_data_4 = document.createElement("td");
+  row_2_data_4.innerHTML = tanniTmp[2];
+  let row_2_data_5 = document.createElement("td");
+  row_2_data_5.innerHTML = tanniTmp[3];
+  let row_2_data_6 = document.createElement("td");
+  row_2_data_6.innerHTML = tanniTmp[4];
+  let row_2_data_7 = document.createElement("td");
+  row_2_data_7.innerHTML = tanniTmp[5];
+  let row_2_data_8 = document.createElement("td");
+  row_2_data_8.innerHTML = tanniTmp[6];
+
+  row_2.appendChild(row_2_data_1);
+  row_2.appendChild(row_2_data_2);
+  row_2.appendChild(row_2_data_3);
+  row_2.appendChild(row_2_data_4);
+  row_2.appendChild(row_2_data_5);
+  row_2.appendChild(row_2_data_6);
+  row_2.appendChild(row_2_data_7);
+  row_2.appendChild(row_2_data_8);
+  tbody.appendChild(row_2);
+
+  let row_3 = document.createElement("tr");
+  let row_3_data_1 = document.createElement("td");
+  row_3_data_1.innerHTML = "修得済単位";
+  let row_3_data_2 = document.createElement("td");
+  row_3_data_2.innerHTML = EarnedCreditsData[0];
+  let row_3_data_3 = document.createElement("td");
+  row_3_data_3.innerHTML = EarnedCreditsData[1];
+  let row_3_data_4 = document.createElement("td");
+  row_3_data_4.innerHTML = EarnedCreditsData[2];
+  let row_3_data_5 = document.createElement("td");
+  row_3_data_5.innerHTML = EarnedCreditsData[3];
+  let row_3_data_6 = document.createElement("td");
+  row_3_data_6.innerHTML = EarnedCreditsData[4];
+  let row_3_data_7 = document.createElement("td");
+  row_3_data_7.innerHTML = EarnedCreditsData[5];
+  let row_3_data_8 = document.createElement("td");
+  row_3_data_8.innerHTML = EarnedCreditsData[6];
+
+  row_3.appendChild(row_3_data_1);
+  row_3.appendChild(row_3_data_2);
+  row_3.appendChild(row_3_data_3);
+  row_3.appendChild(row_3_data_4);
+  row_3.appendChild(row_3_data_5);
+  row_3.appendChild(row_3_data_6);
+  row_3.appendChild(row_3_data_7);
+  row_3.appendChild(row_3_data_8);
+  tbody.appendChild(row_3);
+
+  let row_4 = document.createElement("tr");
+  let row_4_data_1 = document.createElement("td");
+  row_4_data_1.innerHTML = "履修中単位";
+  let row_4_data_2 = document.createElement("td");
+  row_4_data_2.innerHTML = TakingCoursesCreditsData[0];
+  let row_4_data_3 = document.createElement("td");
+  row_4_data_3.innerHTML = TakingCoursesCreditsData[1];
+  let row_4_data_4 = document.createElement("td");
+  row_4_data_4.innerHTML = TakingCoursesCreditsData[2];
+  let row_4_data_5 = document.createElement("td");
+  row_4_data_5.innerHTML = TakingCoursesCreditsData[3];
+  let row_4_data_6 = document.createElement("td");
+  row_4_data_6.innerHTML = TakingCoursesCreditsData[4];
+  let row_4_data_7 = document.createElement("td");
+  row_4_data_7.innerHTML = TakingCoursesCreditsData[5];
+  let row_4_data_8 = document.createElement("td");
+  row_4_data_8.innerHTML = TakingCoursesCreditsData[6];
+
+  row_4.appendChild(row_4_data_1);
+  row_4.appendChild(row_4_data_2);
+  row_4.appendChild(row_4_data_3);
+  row_4.appendChild(row_4_data_4);
+  row_4.appendChild(row_4_data_5);
+  row_4.appendChild(row_4_data_6);
+  row_4.appendChild(row_4_data_7);
+  row_4.appendChild(row_4_data_8);
+  tbody.appendChild(row_4);
+
+  let row_5 = document.createElement("tr");
+  let row_5_data_1 = document.createElement("td");
+  row_5_data_1.innerHTML = "不足単位";
+  let row_5_data_2 = document.createElement("td");
+  row_5_data_2.innerHTML =
+    tanniTmp[0] - EarnedCreditsData[0] - TakingCoursesCreditsData[0];
+  let row_5_data_3 = document.createElement("td");
+  row_5_data_3.innerHTML =
+    tanniTmp[1] - EarnedCreditsData[1] - TakingCoursesCreditsData[1];
+  let row_5_data_4 = document.createElement("td");
+  row_5_data_4.innerHTML =
+    tanniTmp[2] - EarnedCreditsData[2] - TakingCoursesCreditsData[2];
+  let row_5_data_5 = document.createElement("td");
+  row_5_data_5.innerHTML =
+    tanniTmp[3] - EarnedCreditsData[3] - TakingCoursesCreditsData[3];
+  let row_5_data_6 = document.createElement("td");
+  row_5_data_6.innerHTML =
+    tanniTmp[4] - EarnedCreditsData[4] - TakingCoursesCreditsData[4];
+  let row_5_data_7 = document.createElement("td");
+  row_5_data_7.innerHTML =
+    tanniTmp[5] - EarnedCreditsData[5] - TakingCoursesCreditsData[5];
+  let row_5_data_8 = document.createElement("td");
+  row_5_data_8.innerHTML =
+    tanniTmp[6] - EarnedCreditsData[6] - TakingCoursesCreditsData[6];
+
+  var row_5_data_tmp = [
+    row_5_data_1,
+    row_5_data_2,
+    row_5_data_3,
+    row_5_data_4,
+    row_5_data_5,
+    row_5_data_6,
+    row_5_data_7,
+    row_5_data_8,
+  ];
+  for (var j = 0; j < 8; j++) {
+    if (j > 0) {
+      if (
+        tanniTmp[j - 1] -
+          EarnedCreditsData[j - 1] -
+          TakingCoursesCreditsData[j - 1] ==
+        0
+      )
+        row_5_data_tmp[j].style.backgroundColor = "lawngreen";
+      else row_5_data_tmp[j].style.backgroundColor = "hotpink";
+    }
+    row_5.appendChild(row_5_data_tmp[j]);
+  }
+  tbody.appendChild(row_5);
+
+  bar();
+}
+//進捗バーの表示
+var progressData = 0;
+function bar() {
+  for (var i = 0; i < 6; i++) {
+    progressData =
+      Math.round(
+        ((100 * (EarnedCreditsData[i] + TakingCoursesCreditsData[i])) /
+          tanniTmp[i]) *
+          100
+      ) / 100;
+    if (progressData > 100 || isNaN(progressData) == true) progressData = 100;
+    sintyokuData.push(progressData);
+  }
+  sintyokuData.push(
+    Math.round(
+      (100 * sintyokuData.reduce((sum, element) => sum + element, 0) * 100) /
+        600
+    ) / 100
+  );
+
+  document.getElementById("bar").style.display = "block";
+  document.getElementById("barTitle1").innerHTML =
+    "教養必修(" + sintyokuData[0] + "%)";
+  document.getElementById("barTitle2").innerHTML =
+    "教養選択(" + sintyokuData[1] + "%)";
+  document.getElementById("barTitle3").innerHTML =
+    "専門必修(" + sintyokuData[2] + "%)";
+  document.getElementById("barTitle4").innerHTML =
+    "専門選択(" + sintyokuData[3] + "%)";
+  document.getElementById("barTitle5").innerHTML =
+    "選択共通(" + sintyokuData[4] + "%)";
+  document.getElementById("barTitle6").innerHTML =
+    "随意科目(" + sintyokuData[5] + "%)";
+  document.getElementById("barTitle7").innerHTML =
+    "合計単位(" + sintyokuData[6] + "%)";
+
+  document.getElementById("bar1").value = sintyokuData[0];
+  document.getElementById("bar2").value = sintyokuData[1];
+  document.getElementById("bar3").value = sintyokuData[2];
+  document.getElementById("bar4").value = sintyokuData[3];
+  document.getElementById("bar5").value = sintyokuData[4];
+  document.getElementById("bar6").value = sintyokuData[5];
+  document.getElementById("bar7").value = sintyokuData[6];
+}
+//変数を初期化する関数．
+function reset() {
+  kyoyoHissyu = 0;
+  kyoyoSentaku = 0;
+  senmonHissyu = 0;
+  senmonSentaku = 0;
+  sentakuKyotu = 0;
+  zuiiKamoku = 0;
+  gokeiTanni = 0;
+  GraduationSum = 0;
+  EarnedCreditsSum = 0;
+  progressData = 0;
+  TakingCoursesCreditsSum = 0;
+  GPAData.length = 0;
+  EarnedCreditsData.length = 0;
+  TakingCoursesCreditsData.length = 0;
+  sintyokuData.length = 0;
+  tanniTmp.pop();
+}
